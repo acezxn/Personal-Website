2b23a7dc35aef34ce22a7a9a7b433d7731ee7f32
acezxn <acezxn@gmail.com>
2024-05-17 08:11:48 +0800

    Fixed several naming errors

--- src/pages/Secret.js
+++ src/pages/Secret.js
@@ -6,7 +6,7 @@ export const Secret = () => {
             <Navbar />
             <div style={{ height: "calc(40vh - 80px)" }}></div>
             <p style={{ textAlign: "center" }}>
-                <h1 className="monospace" style={{ color: "var(--error-color)" }}>404</h1>
+                <h1 className="monospace" style={{ color: "var(--primary-color)" }}>Hello ^_^</h1>
                 <h2 className="monospace">I guess you found the secret page</h2>
             </p>
             <div style={{ display: "flex", justifyContent: "center" }} >
