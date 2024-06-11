556d93ffa018e7d538be42e8e851b006129ec3f4
acezxn <acezxn@gmail.com>
2024-06-11 12:35:13 +0800

    Added line spacing to skills section

--- src/components/SkillsCard.js
+++ src/components/SkillsCard.js
@@ -12,27 +12,29 @@ export const SkillsCard = () => {
                 }}>Skills</h6>
             <label className="field_left monospace"><b>Programming Languages:</b></label>
             <br />
-            {
-                profile.skills.programming_languages.map((language) => (
-                    <div style={{ display: "inline-block" }}>
-                        <i className={iconmapping[language]} style={{ fontSize: 25, verticalAlign: "middle" }}></i>
-                        <label className="monospace" style={{ paddingLeft: 5, paddingRight: 15 }}>{language}</label>
-                    </div>
-                ))
-            }
-            <br />
+            <div style={{ lineHeight: "30px" }}>
+                {
+                    profile.skills.programming_languages.map((language) => (
+                        <div style={{ display: "inline-block" }}>
+                            <i className={iconmapping[language]} style={{ fontSize: 25, verticalAlign: "middle" }}></i>
+                            <label className="monospace" style={{ paddingLeft: 5, paddingRight: 15 }}>{language}</label>
+                        </div>
+                    ))
+                }
+            </div>
             <hr />
             <label className="field_left monospace"><b>Technologies:</b></label>
             <br />
-            {
-                profile.skills.technologies.map((technology) => (
-                    <div style={{ display: "inline-block" }}>
-                        <i className={iconmapping[technology]} style={{ fontSize: 25, verticalAlign: "middle" }}></i>
-                        <label className="monospace" style={{ paddingLeft: 5, paddingRight: 15 }}>{technology}</label>
-                    </div>
-                ))
-            }
-            <br />
+            <div style={{ lineHeight: "30px" }}>
+                {
+                    profile.skills.technologies.map((technology) => (
+                        <div style={{ display: "inline-block" }}>
+                            <i className={iconmapping[technology]} style={{ fontSize: 25, verticalAlign: "middle" }}></i>
+                            <label className="monospace" style={{ paddingLeft: 5, paddingRight: 15 }}>{technology}</label>
+                        </div>
+                    ))
+                }
+            </div>
             <hr />
         </div>
     )
