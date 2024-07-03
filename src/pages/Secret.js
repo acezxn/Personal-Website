eedbe6ff11212690c7a71dffcbdef21443630956
acezxn <acezxn@gmail.com>
2024-07-03 17:07:56 +0800

    Implemented more 3d banner designs

--- src/pages/Secret.js
+++ src/pages/Secret.js
@@ -1,7 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Navbar } from "../components/Navbar"
 import { Helmet } from "react-helmet";
+import { Canvas, useFrame } from '@react-three/fiber';
 import profile from "./../data/profile.json";
+import theme from "./../data/theme.json"
+
+const SpinningTorus = () => {
+    const mesh = useRef();
+
+    useFrame(() => {
+        if (mesh.current) {
+            mesh.current.rotation.x = 0.5;
+            mesh.current.rotation.y = 1;
+            mesh.current.rotation.z += 0.001;
+        }
+    });
+
+    return (
+        <mesh ref={mesh}>
+            <torusGeometry args={[10, 3, 32, 256]} />
+            <meshBasicMaterial color={theme.common.torus_color} wireframe={true} />
+        </mesh>
+    );
+};
 
 export const Secret = () => {
     const [funFact, setFunFact] = useState("");
@@ -18,17 +39,19 @@ export const Secret = () => {
         <>
             <Helmet><title>^_^</title></Helmet>
             <Navbar />
-            {/* <p style={{ textAlign: "center" }}>
-                <h1 className="monospace" style={{ color: "var(--primary-color)" }}>Hello ^_^</h1>
-                <h3 className="monospace">I guess you found the secret page</h3>
-            </p> */}
-            <div style={{ margin: 10 }}>
+            <div style={{ width: "100vw", height: "100vh", position: "absolute", top: 0, zIndex: 0 }}>
+                <Canvas>
+                    <SpinningTorus />
+                </Canvas>
+            </div>
+            <div style={{ position: "absolute", top: "30vh", width: "100vw", margin: 10 }}>
                 <h3 className="monospace" style={{ color: "var(--primary-color)" }}>You've found me!</h3>
                 <h6 className="monospace">Fun fact</h6>
                 <p className="monospace">{funFact}</p>
                 <h6 className="monospace">What I am doing now</h6>
                 <p className="monospace">{task}</p>
             </div>
+
         </>
     )
 }
