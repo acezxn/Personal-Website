282a7a6027740536037699634998a50ba161e53a
acezxn <acezxn@gmail.com>
2025-05-05 17:48:01 -0400

    Improved projects gallery and updated profile content

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,9 +1,30 @@
 import { Navbar } from "../components/Navbar"
 import { Helmet } from 'react-helmet';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 import profile from "./../data/profile.json";
+import { useEffect, useState } from "react";
 
 export const ProjectsPage = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const renderImage = (item) => {
+        return (
+            <a href={item.original} target="_blank">
+                <img src={item.original} alt={item.originalAlt} style={{ width: width > 800 ? "800px" : "100%" }} />
+            </a>
+        );
+    };
+
+    useEffect(() => {
+        const updateWidth = () => {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", updateWidth);
+        return () => window.removeEventListener("resize", updateWidth);
+    }, []);
+
     return (
         <>
             <Helmet><title>Projects</title></Helmet>
@@ -20,26 +41,27 @@ export const ProjectsPage = () => {
                 </h6>
                 <hr />
                 {
-                    profile.projects.map((project, index) => (
-                        <>
-                            <h6 className="monospace">{project.name}</h6>
-                            <div style={{ padding: 10, overflow: "scroll", width: "calc(100vw - 40px)", maxHeight: "calc(30vh + 150px)" }}>
-                                {
-                                    project.images.map((image, idx) => (
-                                        <a href={image}>
-                                            <img alt="project" src={image} key={idx} style={{ height: "30vh", minHeight: 200, padding: 5, paddingLeft: 0 }} />
-                                        </a>
-                                    ))
-                                }
+                    profile.projects.map((project, index) => {
+                        const photos = project.images.map((image, idx) => {
+                            return {
+                                original: image,
+                                thumbnail: image,
+                            };
+                        });
+                        return (
+                            <div key={index}>
+                                <h6 className="monospace">{project.name}</h6>
+                                <div style={{ margin: 32 }} />
+                                <ImageGallery items={photos} showFullscreenButton={false} renderItem={renderImage} />
+                                <br />
+                                <label className="field monospace">{project.description}</label>
+                                <br />
+                                <br />
+                                <label className="field_left monospace link"><a href={project.link}>[View on Github]</a></label>
+                                <hr />
                             </div>
-                            <br />
-                            <label className="field monospace">{project.description}</label>
-                            <br />
-                            <br />
-                            <label className="field_left monospace link"><a href={project.link}>[View on Github]</a></label>
-                            <hr />
-                        </>
-                    ))
+                        )
+                    })
                 }
             </div>
         </>
